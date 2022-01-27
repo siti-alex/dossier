007b9751fff28b7cf4bc7038f3bb5ffa2eeb499d
lehajuk <asitikovdo@mail.com>
2022-01-27 16:31:33 +0900

    person page

--- src/pages/persons/person/index.vue
+++ src/pages/persons/person/index.vue
@@ -8,15 +8,15 @@
         <div class="row">
           <q-img class="col-3" :src="`http://192.168.202.115:8004/persons/${$route.params.id}/photo`" width="250px" height="250px"></q-img>
           <div class="col-9" style="margin-left: 20px">
-            <q-input class="col" v-model="person.name" label="ФИО" style="margin-right: 20px"/>
-            <q-input class="col" v-model="person.placeOfPost" label="Место работы" style="margin-right: 20px"/>
-            <q-input class="col" v-model="person.post" label="Должность" style="margin-right: 20px"/>
+            <q-input class="col" v-model="person.name" label="ФИО"/>
+            <q-input class="col" v-model="person.placeOfPost" label="Место работы"/>
+            <q-input class="col" v-model="person.post" label="Должность"/>
+            <q-input class="col" v-model="person.dateOfBirth" type="date" label="Дата рождения"/>
           </div>
         </div>
 
         <br>
         <div class="row">
-          <q-input class="col" v-model="person.dateOfBirth" type="date" label="Дата рождения" style="margin-right: 20px"/>
           <q-input class="col" v-model="person.phone" label="Телефон" style="margin-right: 20px"/>
           <q-input class="col" v-model="person.email" label="Email" style="margin-right: 20px"/>
         </div>
@@ -32,18 +32,21 @@
       <br>
 
       <div class="text-h6">Социальные сети</div>
-        <div class="flex">
-          <q-input label="URL"  style="width: 95%; margin-right: 10px" />
-          <q-btn flat icon="add"></q-btn>
+        <div class="flex" v-for="soc in person.socialNetAccs" v-if="person.socialNetAccs.length !== 0">
+          <q-input :label="soc.type" v-model="soc.url" style="width: 100%"/>
         </div>
-
+        <div v-else class="text-subtitle2">
+          Отсутствуют
+        </div>
       <br>
 
       <div class="text-h6">Хобби</div>
-        <q-input
-          filled
-          type="textarea"
-        />
+        <div class="flex" v-for="hob in person.hobbies" v-if="person.hobbies.length !== 0">
+          <q-input :label="hob.type" v-model="hob.url" style="width: 100%"/>
+        </div>
+        <div v-else class="text-subtitle2">
+          Отсутствуют
+      </div>
 
       <br>
 
