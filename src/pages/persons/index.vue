9ad213f08d3acb5ab180f334fc2f07f29c352a98
lehajuk <asitikovdo@mail.com>
2022-01-27 16:14:39 +0900

    person page with photo

--- src/pages/persons/index.vue
+++ src/pages/persons/index.vue
@@ -83,39 +83,6 @@ import { ref } from 'vue'
 import { api } from 'boot/axios'
 import NewPerson from "pages/persons/newPerson";
 
-// const contacts = [ {
-//   id: 1,
-//   name: 'Ruddy Jedrzej',
-//   email: 'rjedrzej0@discuz.net',
-//   letter: 'R'
-// }, {
-//   id: 2,
-//   name: 'Mallorie Alessandrini',
-//   email: 'malessandrini1@marketwatch.com',
-//   letter: 'M'
-// }, {
-//   id: 3,
-//   name: 'Elisabetta Wicklen',
-//   email: 'ewicklen2@microsoft.com',
-//   letter: 'E'
-// }, {
-//   id: 4,
-//   name: 'Seka Fawdrey',
-//   email: 'sfawdrey3@wired.com',
-//   letter: 'S'
-// } ]
-//
-// const offline = [ {
-//   id: 5,
-//   name: 'Brunhilde Panswick',
-//   email: 'bpanswick4@csmonitor.com',
-//   avatar: 'avatar2.jpg'
-// }, {
-//   id: 6,
-//   name: 'Winfield Stapforth',
-//   email: 'wstapforth5@pcworld.com',
-//   avatar: 'avatar6.jpg'
-// } ]
 const columns = [
   {
     name: 'name',
@@ -158,6 +125,12 @@ export default {
           console.log(this.persons);
         })
       },
+    testing() {
+      api.get('/persons/1')
+      .then((response) => {
+        console.log(response);
+      })
+    },
     showNewPerson() {
       this.$refs.newPerson.showDialog();
     },
@@ -168,6 +141,7 @@ export default {
   },
   created() {
     this.loadData();
+    this.testing();
   }
 
 }
--- src/pages/persons/person/index.vue
+++ src/pages/persons/person/index.vue
@@ -1,10 +1,101 @@
 <template>
-<h1>Person</h1>
+  <q-card v-if="person">
+<!--    <q-img :src=""></q-img>-->
+    <q-card-section>
+      <div class="text-h6">Общая информация</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+        <div class="row">
+          <q-img class="col-3" :src="`http://192.168.202.115:8004/persons/${$route.params.id}/photo`" width="250px" height="250px"></q-img>
+          <div class="col-9" style="margin-left: 20px">
+            <q-input class="col" v-model="person.name" label="ФИО" style="margin-right: 20px"/>
+            <q-input class="col" v-model="person.placeOfPost" label="Место работы" style="margin-right: 20px"/>
+            <q-input class="col" v-model="person.post" label="Должность" style="margin-right: 20px"/>
+          </div>
+        </div>
+
+        <br>
+        <div class="row">
+          <q-input class="col" v-model="person.dateOfBirth" type="date" label="Дата рождения" style="margin-right: 20px"/>
+          <q-input class="col" v-model="person.phone" label="Телефон" style="margin-right: 20px"/>
+          <q-input class="col" v-model="person.email" label="Email" style="margin-right: 20px"/>
+        </div>
+        <br>
+        <div class="row">
+          <q-input class="col" v-model="person.passportNumber" label="Номер паспорта" style="margin-right: 20px"/>
+          <q-input class="col" type="date" v-model="person.passportExpiredDate" label="Дата выдачи паспорта" style="margin-right: 20px"/>
+          <q-separator vertical style="margin-right: 20px"/>
+          <q-input class="col" v-model="person.visaNumber" label="Номер Visa" style="margin-right: 20px"/>
+          <q-input class="col" type="date" v-model="person.visaExpiredDate" label="Дата окончания Visa" style="margin-right: 20px"/>
+        </div>
+
+      <br>
+
+      <div class="text-h6">Социальные сети</div>
+        <div class="flex">
+          <q-input label="URL"  style="width: 95%; margin-right: 10px" />
+          <q-btn flat icon="add"></q-btn>
+        </div>
+
+      <br>
+
+      <div class="text-h6">Хобби</div>
+        <q-input
+          filled
+          type="textarea"
+        />
+
+      <br>
+
+      <div class="text-h6">Примечание</div>
+        <q-input
+          filled
+          type="textarea"
+        />
+
+      <hr>
+      <q-btn class="full-width text-white" style="background-color: #8b2639">Сохранить</q-btn>
+
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { api } from 'boot/axios'
 export default {
-  name: "person"
+  name: "person",
+  data: () => ({
+    // serverIp: api,
+    person: null,
+    addUrl: [0],
+    newPerson: {
+      name: null,
+      placeJob: null,
+      job: null,
+      dateBirth: ref('2019-02-01'),
+      telephone: null,
+      email: null,
+      passport: null,
+      datePassword: ref('2019-02-01'),
+      visa: null,
+      dateVisa: ref('2019-02-01'),
+
+      socialNetworks: [],
+    },
+  }),
+  methods: {
+    getPerson() {
+      api.get(`/persons/${this.$route.params.id}`)
+      .then((response) => {
+        this.person = response.data;
+        console.log(this.person);
+      })
+    },
+  },
+  created() {
+    this.getPerson();
+  }
 }
 </script>
 
